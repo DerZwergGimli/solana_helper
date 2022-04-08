c0198fea212d9185b587da343e0bf853b35a225b
DZWG <paule22@protonmail.com>
2022-04-08 15:03:55 +0200

    cleanup

--- solanabeach/src/helper.rs
+++ solanabeach/src/helper.rs
@@ -6,9 +6,18 @@ use reqwest::{Error, Response};
 pub async fn fetch(url: String) -> Result<Response, Error> {
     info!("Fetching: {:?}",url);
     let client = reqwest::Client::new();
-    client.get(url.clone())
-        .header("User-Agent", "Mozilla/5.0")
-        //.bearer_auth(env::var("SOLANABEACH_TOKEN").unwrap())
-        .send()
-        .await
+    match  env::var("SOLANABEACH_TOKEN") {
+        Ok(val) => {client.get(url.clone())
+            .header("User-Agent", "Mozilla/5.0")
+            .bearer_auth(val)
+            .send()
+            .await},
+        _ => {
+            client.get(url.clone())
+                .header("User-Agent", "Mozilla/5.0")
+                .send()
+                .await
+        }
+    }
+
 }
